d7c1f515d1ea1e301140441314a76a7a69b3ffa2
Marcus Wilen <marcus.wilen@hotmail.com>
2020-01-05 18:47:13 +0100

    first commit
    first commit

---
+++ Herokutest/src/test/java/com/example/Herokutest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Herokutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
